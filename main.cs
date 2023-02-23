c53d10674d47a32b4008b5555534c3db74374ea5
ZinchukEk <126098384+ZinchukEk@users.noreply.github.com>
2023-02-23 13:27:07 +0300

    изменен main.cs
    Добавлен алгоритм работы программы + вывод

--- main.cs
+++ main.cs
@@ -13,3 +13,20 @@ namespace MyProgram
             string[] arr2 = new string[arr.Length];
 
             int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length <= 3)
+                {
+                    arr2[count] = arr[i];
+                    count++;
+                }
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(arr2[i]);
+            }
+        }
+    }
+}
