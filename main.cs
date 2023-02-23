6d364d0a6bd63b6da81d0e6d2646f2418be263c9
ZinchukEk <126098384+ZinchukEk@users.noreply.github.com>
2023-02-23 13:25:40 +0300

    Инициализация массивов

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyProgram
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // исходный массив
+            string[] arr = new string[] {"Hello", "How", "Are", "You", "Today"};
+
+            // массив на выходе
+            string[] arr2 = new string[arr.Length];
+
+            int count = 0;
